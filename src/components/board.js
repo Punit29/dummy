9e5ff54eebe541a25912629da4711cf8258b1d3f
Punit Pundir <punitpundir0@gmail.com>
2021-08-06 15:48:30 +0530

    usedp prettier

--- src/components/board.js
+++ src/components/board.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Board = props => {
+const Board = (props) => {
   return (
     <div>
       <Link to={`/board/${props.boards.id}`}>
-        <div className='columncss'>
-          <div className='cardcss'>
+        <div className="columncss">
+          <div className="cardcss">
             <h3>{props.boards.name}</h3>
           </div>
         </div>
