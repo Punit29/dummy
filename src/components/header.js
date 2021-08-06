52d5b383c2a9b180cb75a2b4fff6ef772f57e0b6
Punit Pundir <punitpundir0@gmail.com>
2021-08-06 11:21:13 +0530

    added homepage link in trello image

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,16 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className='header'>
-      <Link to='/'>
-      
-      </Link>
       <Link to='/boards'>
         <button className='btn btn-outline-light boardButton'>Boards</button>
       </Link>
       <div className='trello-img'>
-        <img
-          src='https://a.trellocdn.com/prgb/dist/images/header-logo-2x.01ef898811a879595cea.png'
-          alt='none'
-        />
+        <Link to='/'>
+          <img
+            src='https://a.trellocdn.com/prgb/dist/images/header-logo-2x.01ef898811a879595cea.png'
+            alt='none'
+          />
+        </Link>
       </div>
     </header>
   );
